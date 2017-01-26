9dac0ac325ee61313eaeda7fc3f22b0e201ca62c
Edmond Varga <vedtam@c.local>
2017-01-26 10:02:24 +0200

    command classification

--- bot.py
+++ bot.py
@@ -1,19 +1,41 @@
 import nltk
-from nltk import ne_chunk, pos_tag, word_tokenize
-from nltk.corpus import brown
+from nltk import word_tokenize
 from time import gmtime, strftime
 
-sent_tokenized = word_tokenize('What time it is?')
 
+# user's question
+#sent_tokenized = word_tokenize('please turn on the lights')
+# sent_tokenized = word_tokenize('what time is it?')
+sent_tokenized = word_tokenize('please play some music')
+
+
+# open training set
 train_sents = []
-file = open('document.txt')
+file = open('command_sentences.txt')
 for line in file:
     train_sents.append([nltk.tag.str2tuple(t) for t in line.split()])
 
 
+# train the tagger and tag the input
 unigram_tagger = nltk.UnigramTagger(train_sents)
 tagged_sentence = unigram_tagger.tag(sent_tokenized)
 
-for token in tagged_sentence:
-    if token[0] == "time" and token[1] == "NN":
+
+# handle intent
+def handleIntent(intent):
+    if token[0] == "time":
         print "The time is: " + strftime("%H:%M:%S", gmtime())
+    elif token[0] == "lights":
+        print "The light is turned on"
+    elif token[0] == "music":
+        print "The music is now playing"
+    else:
+        print "Sorry, I don't have a function for the intent: " + intent
+
+
+# check for intents
+for token in tagged_sentence:
+    # is this token a NOUN?
+     if token[1] == "NN":
+        handleIntent(token[0])
+    # TODO: handle verbs for on/off state
